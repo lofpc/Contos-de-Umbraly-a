6e33129f94af9d72ac2796389320b6662d36902d
Lofpc <lofpc1234@gmail.com>
2026-02-19 00:17:20 -0300

    migrei valores de lugar

--- TesteJogoTextov0.1.1.cs
+++ TesteJogoTextov0.1.1.cs
@@ -11,14 +11,8 @@ using Microsoft.VisualBasic;
 //para meu eu do futuro e qualquer um que tentar ler esse código depois. As coisas tavam muito grandes e eu reduzi em troca de legibilidade. Ainda dá, com esforço. Desculpa.
 //procurar dps uma forma melhor de loopar só a tela de status, deve ter. Se alguém ler isso, aceito sugestões, só sou lerdo.
 namespace jogo;class Programa
-{ public class Valores //Fonte para pegar e modificar valores.
-{public static int pontos = 5; public static int mudança = 0; public static int Vida = 3;
-public static string Nome = (""); public static bool Testedesorte = true; public static Random dado = new Random();
-public static int número = dado.Next(1, 21); public static string ReceberDano = "Sua vida desceu em 1 devido à falta de";
-public static List<string> stats = new List<string> {$"Força","Velocidade","Mental","Observação"}; public static List<int> pontostats = [1, 1, 1, 1]; 
-public static int maior = pontostats.Max(); public static int menor = pontostats.Min(); public static int Run = 10; public static int fé = 0;public static int counter = 0; public static int amnr = 0;
-public static int ksy = 0; public static int aqua = 0; public static int revelação = 0; public static int ampliado = 0; public static int tochas = 0; public static int cálice = 0;public static List<string> casting = new List<string> {$""};}  
 
+{
 
 
 public static class Morte //Sistema de morte
--- obj/Debug/net10.0/TesteJogoTexto.AssemblyInfo.cs
+++ obj/Debug/net10.0/TesteJogoTexto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteJogoTexto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e61419305783b202913bc9d21526be705dee720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7d7aff96c59c3131d9ee815dcd8c619b7ea2c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteJogoTexto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteJogoTexto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
